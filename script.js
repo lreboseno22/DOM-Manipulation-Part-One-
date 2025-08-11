d1e661c6ece4ee06da4f40dcc33b02ebd5970dd8
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-11 14:13:19 -0400

    part one complete

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let mainEl = document.querySelector("main")
 console.log(mainEl);
 
 // Set the background color of mainEl to the value stored in the --main-bg CSS custom property
+mainEl.style.backgroundColor = "#4a4e4d";
 
-// Set the background color of mainEl to <h1>DOM Manipulation</h1>.
+// Set the content of mainEl to <h1>DOM Manipulation</h1>
+const newHeader = document.createElement("h1");
+newHeader.textContent = "DOM Manipulation"
+mainEl.append(newHeader);
 
 // Add a class of flex-ctr to mainEl
-
+mainEl.classList.add('flex-ctr');
