c8559af7c473e7a32e6d56a1fb2ecc77b4926fda
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-11 13:52:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Part One:
+
+// Select and cache the <main> element in a variable named mainEl
+let mainEl = document.querySelector("main")
+console.log(mainEl);
+
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property
+
+// Set the background color of mainEl to <h1>DOM Manipulation</h1>.
+
+// Add a class of flex-ctr to mainEl
+
